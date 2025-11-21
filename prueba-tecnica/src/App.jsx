b3ddc95365ccb91a966b76e4af17364ba1e6c6a9
Lara CV <lara.cv@tutamail.com>
2025-11-21 11:47:48 +0100

    Finalización tareas iniciales (imagen y palabra)

--- prueba-tecnica/src/App.jsx
+++ prueba-tecnica/src/App.jsx
@@ -1,20 +1,71 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import "./App.css";
+
+const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?json=true`;
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 export function App() {
-  const [fact, setFact] = useState("Test");
+  const [fact, setFact] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  useEffect(() => {
+  /*useEffect(() => {
     //Fact
-    fetch("https://catfact.ninja/fact")
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((response) => response.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstWord = fact.split(" ")[0];
+        //const nFirstWords = fact.split(" ").slice(0, 3).join(" ");
+
+        //Image
+        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+          .then((response) => response.json())
+          .then((data) => setImageUrl(data.url));
+      });
+  }, []);*/
+
+  //Alternativa: varios useEffect
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((response) => response.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
   }, []);
+  
+  useEffect(() => {
+    if(!fact) return;
+    const firstWord = fact.split(" ")[0];  
+    fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+      .then((response) => response.json())
+      .then((data) => setImageUrl(data.url));
+  }, [fact]);
 
   return (
-    <>
+    <main>
       <h1>App de gatitos</h1>
-      <p>Hecho: {fact}</p>
-      <p>Primera palabra: {fact.split(" ")[0]}</p>
-    </>
+      <section id="seccion1">
+        {fact && <p>Hecho: {fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={`Cat image saying the first word of '${fact}'`}
+          />
+        )}
+      </section>
+      <section id="seccion2">
+        {fact && <p>Hecho: {fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={`Cat image saying the first word of '${fact}'`}
+          />
+        )}
+      </section>
+    </main>
   );
 }
